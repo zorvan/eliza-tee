c828d62d0d4ad3fde96a9722a267f7946850a187
zorvan <nonlinear_dynamic@yahoo.com>
2024-12-17 00:54:22 +0000

    env and nvm

--- enclave/docker-run.sh
+++ enclave/docker-run.sh
@@ -36,7 +36,33 @@ PACKAGES=(
 )
 
 # Start building the docker run command
-CMD="docker run --platform linux/amd64 -p 3000:3000 --device /dev/sgx_enclave -v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket -d"
+CMD="docker run \
+    --platform linux/amd64 \
+    -p 3000:3000 \
+    --device /dev/sgx_enclave \
+    -v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket \
+    -e OPENAI_API_KEY="" \
+    -e REDPILL_API_KEY="" \
+    -e ELEVENLABS_XI_API_KEY="" \
+    -e ELEVENLABS_MODEL_ID=eleven_multilingual_v2 \
+    -e ELEVENLABS_VOICE_ID=21m00Tcm4TlvDq8ikWAM \
+    -e ELEVENLABS_VOICE_STABILITY=0.5 \
+    -e ELEVENLABS_VOICE_SIMILARITY_BOOST=0.9 \
+    -e ELEVENLABS_VOICE_STYLE=0.66 \
+    -e ELEVENLABS_VOICE_USE_SPEAKER_BOOST=false \
+    -e ELEVENLABS_OPTIMIZE_STREAMING_LATENCY=4 \
+    -e ELEVENLABS_OUTPUT_FORMAT=pcm_16000 \
+    -e TWITTER_DRY_RUN=false \
+    -e TWITTER_USERNAME="" \
+    -e TWITTER_PASSWORD="" \
+    -e TWITTER_EMAIL="" \
+    -e X_SERVER_URL=https://api.red-pill.ai/v1 \
+    -e SOL_ADDRESS=So11111111111111111111111111111111111111112 \
+    -e SLIPPAGE=1 \
+    -e RPC_URL=https://api.mainnet-beta.solana.com \
+    -e SERVER_PORT=3000 \
+    -e WALLET_SECRET_SALT=secret_salt \
+    -d"
 
 # Add base mounts
 for mount in "${BASE_MOUNTS[@]}"; do
