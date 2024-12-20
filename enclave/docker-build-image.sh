dc691165ac74fe5aef12104ee1177eb56985a50c
zorvan <nonlinear_dynamic@yahoo.com>
2024-12-20 14:34:12 +0000

    fix all errors up to wasm.instantiate

--- enclave/docker-build-image.sh
+++ enclave/docker-build-image.sh
@@ -1,13 +1,13 @@
-#docker system prune -f
-#docker builder prune -f
+docker system prune -f
+docker builder prune -f
 
-read -p "Building version [>= v0.1.6-alpha.1] : " ver
+read -p "Building version [v0.1.6-alpha.4] : " ver
 
-VER=${ver:-v0.1.6-alpha.1}
+VER=${ver:-v0.1.6-alpha.4}
 
 #--progress=plain
 #--rm --no-cache
-docker build  --platform linux/amd64 \
+docker build  --rm --no-cache --platform linux/amd64 \
     -t eliza-sgx:$VER \
     -t eliza-sgx:latest \
     --build-arg UBUNTU_VERSION=22.04 \
---
+++ enclave/gramine/copy.sh
@@ -0,0 +1,22 @@
+echo "======= Removing old "
+#rm -rf ./package.json
+#rm -rf ./pnpm-workspace.yaml
+#rm -rf ./turbo.json
+rm -rf ./node_modules
+rm -rf ./agent
+rm -rf ./packages
+rm -rf ./scripts
+rm -rf ./characters
+
+echo "======= Copying new "
+#cp -rf /eliza-src/package.json .
+#cp -rf /eliza-src/pnpm-workspace.yaml .
+#cp -rf /eliza-src/.npmrc ./
+#cp -rf /eliza-src/turbo.json .
+cp -rf /eliza-src/node_modules .
+cp -rf /eliza-src/agent .
+cp -rf /eliza-src/packages .
+cp -rf /eliza-src/scripts .
+cp -rf /eliza-src/characters .
+
+echo "======= Done "
